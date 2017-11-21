8937ebc8bee5c4e5e79e9044f672f72d39f98e63
muoitm <muoimouse@gmail.com>
2017-11-21 22:17:10 +0700

    update

--- app.js
+++ app.js
@@ -21,7 +21,6 @@ db.once('open', function () {
     console.log('Connect mongoDB is successfully')
 })
 
-
 // view engine setup
 // app.set('views', path.join(__dirname, 'views'));
 // app.set('view engine', 'ejs');
